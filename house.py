6d59efec370bfd8218763ade4d8f1c16e5d934f7
Anshika Singh <singhanshika29.as@gmail.com>
2026-03-25 15:42:59 +0530

    Update house.py

--- house.py
+++ house.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("House Price Prediction")
+st.title("🏠 House Price Prediction App")
 
 area = st.number_input("Enter area (sq ft)")
 bedrooms = st.number_input("Enter number of bedrooms")
 
 if st.button("Predict"):
     price = area * 1000 + bedrooms * 50000
-    st.success(f"Predicted Price: ₹ {price}")
+    st.success(f"Predicted Price: ₹ {price:,.0f}")
